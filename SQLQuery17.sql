a3ae3e85ccd34e914d0afee2ba3be69d8b9f0684
ThaisCooke <98999262+ThaisCooke@users.noreply.github.com>
2022-02-10 23:29:18 -0500

    Add files via upload

---
+++ SQLQuery17.sql
@@ -0,0 +1,28 @@
+--CREATE TABLE CitiesGOT
+--(Nameofcharacter TEXT,
+--City TEXT,
+--FamilyRuler TEXT);
+
+--INSERT INTO CitiesGOT VALUES
+--('Sansa', 'Winterfell', 'Stark')
+
+--INSERT INTO CitiesGOT VALUES
+--('Arya', 'Winterfell', 'Stark'),
+--('Bran', 'Winterfell', 'Stark'),
+--('Robb', 'Winterfell', 'Stark'),
+--('Ned', 'Winterfell', 'Stark'),
+--('Rickon', 'Winterfell', 'Stark')
+
+--INSERT INTO CitiesGOT VALUES
+--('Daenerys', 'Dragonstone', 'Baratheon'),
+--('Jon Snow', 'Winterfell', 'Stark'),
+--('Viserys', 'Dragonstone', 'Baratheon'),
+--('Cersei', 'Casterly Rock', 'Lannister')
+
+--INSERT INTO CitiesGOT VALUES
+--('Catelyn', 'Winterfell', 'Stark'),
+--('Littlefinger', 'The Eerye', 'Arryn'),
+--('Tyrion', 'Casterly Rock', 'Lannister')
+
+--INSERT INTO CitiesGOT VALUES
+--('Davos', 'Kings Landing', 'Baratheon')
