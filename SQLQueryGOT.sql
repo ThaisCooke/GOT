6991cf8ffa01c1b4ad7c67f5a6729ea429872f58
ThaisCooke <98999262+ThaisCooke@users.noreply.github.com>
2022-03-24 16:53:15 -0400

    Update SQLQueryGOT.sql

--- SQLQueryGOT.sql
+++ SQLQueryGOT.sql
@@ -19,14 +19,14 @@ INSERT INTO GOTProject VALUES
 ('Rickon', 'House Stark', 'North', 1, 6, 'Ramsey Bolton', 'shooting arrow', 13)
 
 INSERT INTO GOTProject VALUES
-('Daenerys', 'House Targaryen', 'Kings Landing', 1, 6, 'Jon Snow', 'Knife', 524),
+('Daenerys', 'House Targaryen', 'Crownlands', 1, 6, 'Jon Snow', 'Knife', 524),
 ('Jon Snow', 'House Stark', 'North', 1, 0, 'none', 'none', 651),
-('Viserys', 'House Targaryen', 'Kings Landing', 1, 1, 'Khal Drogo', 'molten gold', 21)
+('Viserys', 'House Targaryen', 'Crownlands', 1, 1, 'Khal Drogo', 'molten gold', 21)
 
 INSERT INTO GOTProject VALUES
 ('Cersei', 'House Lannister', 'Westerlands', 1, 8, 'none', 'buried', 425),
-('Cersei', 'House Baratheon', 'Kings Landing', 1, 8, 'none', 'buried', 425),
-('Jon Snow', 'House Targaryen', 'Kings Landing', 1, 0, 'none', 'none', 651),
+('Cersei', 'House Baratheon', 'Crownlands', 1, 8, 'none', 'buried', 425),
+('Jon Snow', 'House Targaryen', 'Crownlands', 1, 0, 'none', 'none', 651),
 ('Catelyn', 'House Stark', 'North', 1, 3, 'Raymund Frey', 'knife', 83),
 ('Tyrion', 'House Lannister', 'Westerlands', 1, 8, 'none', 'none', 697),
 ('Sansa', 'House Lannister', 'Westerlands', 1, 0, 'none', 'none', 418),
@@ -49,7 +49,7 @@ INSERT INTO GOTProject Values
 
 INSERT INTO GOTProject Values
 ('Theon', 'House Greyjoy', 'Iron Islands', 1, 8, 'Night King', 'spear', 261),
-('Yara', 'House Greyjoy', 'Iron Islands', 2, 8, 'none', 'none', 0),
+('Yara', 'House Greyjoy', 'Iron Islands', 2, 8, 'none', 'none', 32),
 ('Euron', 'House Greyjoy', 'Iron Islands', 6, 8, 'Jaime Lannister', 'duel', 20),
 ('Balon', 'House Greyjoy', 'Iron Islands', 2, 6, 'Euron Grejoy', 'Fall', 8)
 
@@ -63,7 +63,7 @@ DELETE FROM [GOTProject] WHERE nameofcharacter = 'Sansa'
 INSERT INTO [GOTProject] VALUES
 ('Sansa', 'House Stark', 'North', 1, 0, 'none', 'none', 418)
 
-'Jon Snow', 'House Stark', 'North', 1, 0, 'none', 'none', 651
+'Jon Snow', 'House Stark', 'North', 1, 0, 'none', 'none', 651)
 
 DELETE FROM [GOTProject] WHERE nameofcharacter = 'Robin'
 
@@ -77,18 +77,18 @@ INSERT INTO [GOTProject] VALUES
 ('Jorah', 'House Mormont', 'North', 1, 8, 'night walkers', 'stabbed', 143)
 
 INSERT INTO [GOTProject] VALUES
-  ('Joffrey', 'House Baratheon', 'Kings Landing', 1, 4, 'Oleanna Tyrell', 'poisoned', 70),
-  ('Sandor (The Hound)', 'House Clegane', 'Kings Landing', 1, 8, 'Gregor (The Mountain)', 'Battle', 231),
-  ('Gregor (The Mountain)', 'House Clegane', 'Kings Landing', 1, 8, 'Sandor (The Hound)', 'Battle', 34),
-  ('Davos', 'House Seaworth', 'Kings Landing', 2, 8, 'none', 'none', 296),
+  ('Joffrey', 'House Baratheon', 'Crownlands', 1, 4, 'Oleanna Tyrell', 'poisoned', 70),
+  ('Sandor (The Hound)', 'House Clegane', 'Crownlands', 1, 8, 'Gregor (The Mountain)', 'Battle', 231),
+  ('Gregor (The Mountain)', 'House Clegane', 'Crownlands', 1, 8, 'Sandor (The Hound)', 'Battle', 34),
+  ('Davos', 'House Seaworth', 'Crownlands', 2, 8, 'none', 'none', 296),
   ('Samwell', 'House Tarly', 'Reach', 1, 8, 'none', 'none', 264),
   ('Stannis', 'House Baratheon', 'Stormlands', 2, 5, 'Brienne of Tarth', 'beheaded', 73),
   ('Melisandre', 'None', 'Essos', 2, 8, 'old age', 'none', 73)
   
   INSERT INTO [GOTProject] VALUES
   ('Jeor', 'House Mormont', 'North', 1, 3, 'Rast', 'Stabbed', 24),
-  ('Bronn', 'None', 'Kings Landing', 1, 0, 'none', 'none', 83),
-  ('Varys', 'House Targaryen', 'Kings Landing', 1, 0, 'Daenerys Targaryen', 'Burned', 267)
+  ('Bronn', 'None', 'Crownlands', 1, 0, 'none', 'none', 83),
+  ('Varys', 'House Targaryen', 'Crownlands', 1, 0, 'Daenerys Targaryen', 'Burned', 267)
   
   INSERT INTO [GOTProject] VALUES
   ('Shae', 'None', 'Essos', 1, 4, 'Tyrion Lannister', 'Strangled', 50),
@@ -137,11 +137,11 @@ INSERT INTO [GOTProject] VALUES
   ('Daario', 'House Targaryen', 'Essos', 3, 0, 'none', 'none', 46),
   ('Missandei', 'House Targaryen', 'Essos', 3, 0, 'Cersei Lannister', 'Beheaded', 228),
   ('Ellaria', 'House Martell', 'Dorne', 4, 7, 'Cersei Lannister', 'Unknown', 24),
-  ('Tommen', 'House Baratheon', 'Kings Landing', 1, 6, 'Tommen Baratheon', 'Suicide', 43),
-  ('Myrcella', 'House Baratheon', 'Kings Landing', 1, 6, 'Ellaria Sand', 'Poisoned', 12),
+  ('Tommen', 'House Baratheon', 'Crownlands', 1, 6, 'Tommen Baratheon', 'Suicide', 43),
+  ('Myrcella', 'House Baratheon', 'Crownlands', 1, 6, 'Ellaria Sand', 'Poisoned', 12),
   ('Jaqen Hgahr', 'none', 'Essos', 1, 0, 'none', 'none', 27),
   ('Roose', 'House Bolton', 'North', 2, 6, 'Ramsay Bolton', 'Stabbed', 30),
-  ('High Sparrow', 'None', 'Kings Landing', 5, 6, 'Cersei Lannister', 'Wildfire Explosion', 38),
+  ('High Sparrow', 'None', 'Crownlands', 5, 6, 'Cersei Lannister', 'Wildfire Explosion', 38),
   ('Grey Worm', 'House Targaryen', 'Essos', 3, 0, 'none', 'none', 179)
 
 INSERT INTO [GOTProject] VALUES
@@ -171,9 +171,9 @@ INSERT INTO [GOTProject] VALUES
 ('Xaro Xhoan Daxos', 'None', 'Essos', 2, 2, 'Daenerys Targaryen', 'Unknown', 11),
 ('Mirri Maz Duur', 'None', 'Essos', 1, 1, 'Daenerys Targaryen', 'Burned', 8),
 ('Podrick', 'House Payne', 'Westerland', 2, 0, 'None', 'None', 45),
-('Ros', 'None', 'Kings Landing', 1, 3, 'Joffrey Lannister', 'Arrow Shot', 22),
-('Qyburn', 'House Lannister', 'Kings Landing', 3, 8, 'Gregor (The Mountain)', 'Skull Crushed', 25),
-('Pycelle', 'House Baratheon', 'Kings Landing', 1, 6, 'Qyburn', 'stabbed', 35),
+('Ros', 'None', 'Crownlands', 1, 3, 'Joffrey Lannister', 'Arrow Shot', 22),
+('Qyburn', 'House Lannister', 'Crownlands', 3, 8, 'Gregor (The Mountain)', 'Skull Crushed', 25),
+('Pycelle', 'House Baratheon', 'Crownlands', 1, 6, 'Qyburn', 'stabbed', 35),
 ('Benjen', 'House Stark', 'North', 1, 7, 'Night Walkers', 'Overwhelmed', 13),
 ('Alliser', 'House Thorne', 'North', 1, 6, 'Hanged', 'Jon Snow', 26),
 ('Olly', 'Nights Watch', 'North', 4, 6, 'Hanged', 'Jon Snow', 12),
@@ -188,7 +188,7 @@ INSERT INTO [GOTProject] VALUES
 
 INSERT INTO [GOTProject] VALUES
 ('Lancel', 'House Lannister', 'Westerlands', 1, 6, 'Cersei Lannister', 'Wildfire Explosion', 19),
-('Lyanna', 'House Mormont', 'North', 6, 8, 'Night walker', 'Crushed', 10)
+('LyannaM', 'House Mormont', 'North', 6, 8, 'Night walker', 'Crushed', 10)
 
 -- CREATING NEW TABLE THAT INCLUDES THE CITIES AND GENDER OF CHARACTERS
 
@@ -259,7 +259,7 @@ INSERT INTO CitiesGOT VALUES
 ('Trystane', 'Sunspear', 'Male'),
 ('Doran', 'Sunspear', 'Male'),
 ('Oberyn', 'Sunspear', 'Male'),
-('Lyanna', 'Winterfell', 'Female'),
+('LyannaS', 'Winterfell', 'Female'),
 ('Hodor', 'Winterfell', 'Male'),
 ('Oleanna', 'Highgarden', 'Female')
 
@@ -289,7 +289,7 @@ INSERT INTO CitiesGOT VALUES
 
 INSERT INTO CitiesGOT VALUES
 ('Lancel', 'Kings Landing', 'Male'),
-('Lyanna', 'Bear Island', 'Female'),
+('LyannaM', 'Bear Island', 'Female'),
 ('Ramsey', 'Dreadfort', 'Male'),
 ('Gilly', 'Crasters Keep', 'Female'),
 ('Daario', 'Tyrosh', 'Male'),
@@ -308,3 +308,44 @@ GOTProject.killer, GOTProject.method, GOTProject.screentime, CitiesGOT.City, Cit
 FROM GOTProject
 INNER JOIN CitiesGOT ON GOTProject.Nameofcharacter = CitiesGOT.Nameofcharacter
 
+-- COUNT HOW MANY CHARACTERS BY GENDER
+SELECT gender, COUNT (Gender)
+FROM CitiesGOT
+GROUP BY Gender 
+
+-- WHO IS ALIVE OR DECEASED BY THE END OF THE SHOW
+SELECT Nameofcharacter,
+CASE
+WHEN Seasonwhendied = 0 THEN 'alive'
+ELSE 'Deceased'
+END AS 'Status'
+FROM GOTProject;
+
+--MOST POPULAR WAY CHARACTERS DIED
+-- Disclosure: I didn't have enough Data for minor characters, such as the Frey Family. So, they were not counted and not part of this list.
+If those were counted, probably the number of poisoned/stabbed would be much higher
+SELECT method, COUNT(method)
+FROM GOTProject
+GROUP BY method 
+ORDER BY 2 DESC
+
+-- MOST SCREENTIME
+--Disclosure: Those were approximate numbers, since the data was hard to find, and a lot of times it didn't include the last season. 
+I can say that at least the top 15 are accurate.
+SELECT Nameofcharacter, screentime
+FROM GOTProject
+ORDER BY 2 DESC
+
+-- WHICH CITY ARE THE CHARACTERS FROM
+SELECT City, COUNT(City)
+FROM CitiesGOT
+GROUP BY city
+ORDER BY 2 DESC
+
+--WHO KILLED MOST PEOPLE
+-- Disclosure: Again, I didn't have enough Data for minor characters. So, they were not counted and not part of this list.
+If those were counted, probably the killer would have killed many more people
+SELECT killer, COUNT(killer)
+FROM GOTProject
+GROUP BY killer 
+ORDER BY 2 DESC
